0f23f2a45f6682460956663c2812a474f783970e
HP <seyfeddinonur055@gmail.com>
2025-02-07 18:29:21 +0300

    Anasayfa oluşturuldu

---
+++ eyw/src/AnaSayfa.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+public class AnaSayfa {
+
+        public AnaSayfa(){
+            JFrame frame = new JFrame("Ana Sayfa");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setSize(400, 300);
+            frame.setLayout(null);
+            frame.setVisible(true);
+
+        }
+
+    }
+
+
